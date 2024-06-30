61887a1db89c7caac0b83213f74f81ef7507c02c
Arck <Arck.com>
2024-06-30 17:38:25 +0300

    Первый коммит

---
+++ cpp_mod1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    //переменные, которые содержат значения чисел Фибоначчи до 5
+    double f1 = 0.0;
+    double f2 = 1.0;
+    double f3 = 1.0;
+    double f4 = 2.0;
+    double f5 = 3.0;
+
+    //вывод первых 5 чисел Фибоначчи
+    std::cout << "Fibonacchi number 1 :" << f1 << std::endl;
+    std::cout << "Fibonacchi number 2 :" << f2 << std::endl;
+    std::cout << "Fibonacchi number 3 :" << f3 << std::endl;
+    std::cout << "Fibonacchi number 4 :" << f4 << std::endl;
+    std::cout << "Fibonacchi number 5 :" << f5 << std::endl;
+}
